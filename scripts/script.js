fe091d1d8d2d462a92d4aab8efa4187ecfed6347
Lakshiya-CS <lsenavaraiyanlavanya@mail.weber.edu>
2025-09-07 00:16:15 -0600

    doen with Lab2

--- scripts/script.js
+++ scripts/script.js
@@ -1,17 +1,14 @@
 function setTheme(mode) {
     if (mode === "dark") {
-        document.getElementById("mainPage").style.backgroundColor = "black";
-        document.getElementById("mainPage").style.color = "white";
+        document.getElementById("mainPage").style="background-color:black;color:white"
         document.getElementById("settings").innerHTML = "Current Setting: Dark Mode";
     } 
     else if (mode === "light") {
-        document.getElementById("mainPage").style.backgroundColor = "white";
-        document.getElementById("mainPage").style.color = "black";
+        document.getElementById("mainPage").style="background-color:white;color:black"
         document.getElementById("settings").innerHTML = "Current Setting: Light Mode";
     } 
     else {
-        document.getElementById("mainPage").style.backgroundColor = "#d9b8f5";
-        document.getElementById("mainPage").style.color = "#70569e";
+        document.getElementById("mainPage").style="background-color:#d9b8f5;color:#70569e";
         document.getElementById("settings").innerHTML = "Current Setting: Default Mode";
     }
 }
