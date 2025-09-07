84c89b1bf221b6d981d51b03074c9557728c2a7c
Lakshiya-CS <lsenavaraiyanlavanya@mail.weber.edu>
2025-09-06 23:23:13 -0600

    made changes

---
+++ scripts/script.js
@@ -0,0 +1,17 @@
+function setTheme(mode) {
+    if (mode === "dark") {
+        document.getElementById("mainPage").style.backgroundColor = "black";
+        document.getElementById("mainPage").style.color = "white";
+        document.getElementById("settings").innerHTML = "Current Setting: Dark Mode";
+    } 
+    else if (mode === "light") {
+        document.getElementById("mainPage").style.backgroundColor = "white";
+        document.getElementById("mainPage").style.color = "black";
+        document.getElementById("settings").innerHTML = "Current Setting: Light Mode";
+    } 
+    else {
+        document.getElementById("mainPage").style.backgroundColor = "#f3f0f4";
+        document.getElementById("mainPage").style.color = "#70569e";
+        document.getElementById("settings").innerHTML = "Current Setting: Default Mode";
+    }
+}
